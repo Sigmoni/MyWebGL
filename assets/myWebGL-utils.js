e9732c1622c692b567d4c4681b68efcc95efeb51
mck <sigmoni@163.com>
2020-11-16 12:25:45 +0800

    Simplifies the Code of test.js

---
+++ assets/myWebGL-utils.js
@@ -0,0 +1,71 @@
+"use strict";
+
+function createShader(gl_o, type, source) {
+    let gl = WebGLRenderingContext;
+    gl = gl_o;
+
+    var shader = gl.createShader(type);
+    gl.shaderSource(shader, source);
+    gl.compileShader(shader);
+
+    var success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
+    if (success){
+        return shader;
+    }
+
+    console.log(gl.getShaderInfoLog(shader));
+    gl.deleteShader(shader);
+}
+
+function createProgram(gl_o, vertexShader, fragmentShader) {
+    let gl = WebGLRenderingContext;
+    gl = gl_o;
+
+    var program = gl.createProgram();
+    gl.attachShader(program, vertexShader);
+    gl.attachShader(program, fragmentShader);
+    gl.linkProgram(program);
+
+    var success = gl.getProgramParameter(program, gl.LINK_STATUS);
+    if (success) {
+        return program;
+    }
+
+    console.log(gl.getProgramInfoLog(program));
+    gl.deleteProgram(program);
+}
+
+function resizeCanvasToDisplaySize(canvas, multiplier) {
+    multiplier = multiplier || 1;
+    const width  = canvas.clientWidth  * multiplier | 0;
+    const height = canvas.clientHeight * multiplier | 0;
+    if (canvas.width !== width ||  canvas.height !== height) {
+      canvas.width  = width;
+      canvas.height = height;
+      return true;
+    }
+    return false;
+  }
+
+function getShaderSource(filename) {
+    var xmlhttp = new XMLHttpRequest();
+
+    xmlhttp.open("GET", filename, false);
+    xmlhttp.send();
+    return xmlhttp.responseText;
+}
+
+function programInit(gl_o, src_vs, src_fs) {
+    var gl = WebGLRenderingContext;
+    gl = gl_o;
+
+    var vertexShaderSource = getShaderSource(src_vs);
+    var fragmentShaderSource = getShaderSource(src_fs);
+
+    var vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexShaderSource);
+    var fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSource);
+
+    var program = createProgram(gl, vertexShader, fragmentShader);
+
+    return program;
+}
--- test/test.js
+++ test/test.js
@@ -1,60 +1,5 @@
 "use strict";
 
-function createShader(gl_o, type, source) {
-    let gl = WebGLRenderingContext;
-    gl = gl_o;
-
-    var shader = gl.createShader(type);
-    gl.shaderSource(shader, source);
-    gl.compileShader(shader);
-
-    var success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
-    if (success){
-        return shader;
-    }
-
-    console.log(gl.getShaderInfoLog(shader));
-    gl.deleteShader(shader);
-}
-
-function createProgram(gl_o, vertexShader, fragmentShader) {
-    let gl = WebGLRenderingContext;
-    gl = gl_o;
-
-    var program = gl.createProgram();
-    gl.attachShader(program, vertexShader);
-    gl.attachShader(program, fragmentShader);
-    gl.linkProgram(program);
-
-    var success = gl.getProgramParameter(program, gl.LINK_STATUS);
-    if (success) {
-        return program;
-    }
-
-    console.log(gl.getProgramInfoLog(program));
-    gl.deleteProgram(program);
-}
-
-function resizeCanvasToDisplaySize(canvas, multiplier) {
-    multiplier = multiplier || 1;
-    const width  = canvas.clientWidth  * multiplier | 0;
-    const height = canvas.clientHeight * multiplier | 0;
-    if (canvas.width !== width ||  canvas.height !== height) {
-      canvas.width  = width;
-      canvas.height = height;
-      return true;
-    }
-    return false;
-  }
-
-function getShaderSource(filename, id) {
-    var xmlhttp = new XMLHttpRequest();
-
-    xmlhttp.open("GET", filename, false);
-    xmlhttp.send();
-    document.getElementById(id).innerHTML = xmlhttp.responseText;
-}
-
 function main() {
     const canvas = document.querySelector("#canvas");
     const gl_o = canvas.getContext('webgl');
@@ -66,16 +11,7 @@ function main() {
         return;
     }
 
-    getShaderSource("test.vs", "VertShader");
-    getShaderSource("test.fs", "FragShader");
-    
-    var vertexShaderSource = document.querySelector('#VertShader').text;
-    var fragmentShaderSource = document.querySelector('#FragShader').text;
-
-    var vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexShaderSource);
-    var fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSource);
-
-    var program = createProgram(gl, vertexShader, fragmentShader);
+    var program = programInit(gl, "test.vs", "test.fs");
 
     var positionAttributeLocation = gl.getAttribLocation(program, "a_position");
 
